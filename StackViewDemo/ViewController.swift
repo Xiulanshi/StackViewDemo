0af3bbaf7ed445c771ddc6f116be5384f972fcf9
Xiulan Shi <xiulanshi2013@gmail.com>
2016-01-21 15:34:56 -0500

    First Commit

---
+++ StackViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Xiulan Shi on 1/21/16.
+//  Copyright © 2016 Xiulan Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
